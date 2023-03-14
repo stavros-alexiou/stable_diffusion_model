e6a24338a6426151d668cb042d0d7937fc8c746c
Stavros El. Alexiou <stavros.alexiou13@gmail.com>
2023-03-14 15:16:05 +0200

    final

--- denoising.py
+++ denoising.py
@@ -1,21 +1,21 @@
-# Make a folder to store results
-# !rm -rf /content/drive/MyDrive/stablediff/electric
-# !mkdir -p /content/drive/MyDrive/stablediff/electric
-
-# Some settings
-#prompt = ["a futuristic city, abandoned and overgrown with plants, dystopia, bathed in sunlight"]
-prompt = ["an electric sky city on an alien world"]
-height = 512                        # default height of Stable Diffusion
-width = 768                         # default width of Stable Diffusion
-num_inference_steps = 50            # Number of denoising steps
-guidance_scale = 8.0                # Scale for classifier-free guidance
-generator = torch.manual_seed(1200)   # Seed generator to create the inital latent noise
+prompt = 'a futuristic city, abandoned and overgrown with plants, dystopia, bathed in sunlight'
+height = 512                     
+width = 768 
+num_inference_steps = 100 
+guidance_scale = 8     
+generator = torch.manual_seed(10) 
 batch_size = 1
 
+# Make a folder to store results
+# !rm -rf /content/drive/MyDrive/stablediff/denoising2/
+# !mkdir -p /content/drive/MyDrive/stablediff/denoising2/
+
 # Prep text 
-text_input = tokenizer(prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
+text_input = tokenizer([prompt], padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
 with torch.no_grad():
   text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
+
+# And the uncond. input as before:
 max_length = text_input.input_ids.shape[-1]
 uncond_input = tokenizer(
     [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
@@ -27,45 +27,46 @@ text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
 # Prep Scheduler
 scheduler.set_timesteps(num_inference_steps)
 
-for idx in range(100):
-    generator = torch.manual_seed(idx)
+# Prep latents
+latents = torch.randn(
+  (batch_size, unet.in_channels, height // 8, width // 8),
+  generator=generator,
+)
+latents = latents.to(torch_device)
+latents = latents * scheduler.sigmas[0] # Need to scale to match k
 
-    # Prep latents
-    latents = torch.randn(
-    (batch_size, unet.in_channels, height // 8, width // 8),
-    generator=generator,
-    )
-    latents = latents.to(torch_device)
-    latents = latents * scheduler.sigmas[0] # Need to scale to match k
-
-    # Loop
-    with autocast("cuda"):
-        for i, t in tqdm(enumerate(scheduler.timesteps)):
-            # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
-            latent_model_input = torch.cat([latents] * 2)
-            sigma = scheduler.sigmas[i]
-            latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
-
-            # predict the noise residual
-            with torch.no_grad():
-                noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings)["sample"]
-
-            # perform guidance
-            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
-
-            # compute the previous noisy sample x_t -> x_t-1
-            latents = scheduler.step(noise_pred, i, latents)["prev_sample"]
-
-    # scale and decode the image latents with vae
-    latents = 1 / 0.18215 * latents
-
-    with torch.no_grad():
-        image = vae.decode(latents)
+# Loop
+with autocast("cuda"):
+  for i, t in tqdm(enumerate(scheduler.timesteps)):
+    # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
     
-    # Display
-    image = (image / 2 + 0.5).clamp(0, 1)
-    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
-    images = (image * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-    pil_images[0].save(f'/content/drive/MyDrive/stablediff/electric/{idx:04}.jpeg')
+    im_input = latents_to_pil(latents)[0]
+
+    latent_model_input = torch.cat([latents] * 2)
+    sigma = scheduler.sigmas[i]
+    latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
+
+    # predict the noise residual
+    with torch.no_grad():
+      noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings)["sample"]
+
+    # perform guidance
+    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+    
+    # Get the predicted x0:
+    latents_x0 = latents - sigma * noise_pred
+
+    im_t0 = latents_to_pil(latents_x0)[0]
+    im_noise = latents_to_pil(sigma * noise_pred)[0]
+
+    # And the previous noisy sample x_t -> x_t-1
+    latents = scheduler.step(noise_pred, i, latents)["prev_sample"]
+    im_next = latents_to_pil(latents)[0]
+
+    # Combine the two images and save for later viewing
+    im = Image.new('RGB', (2304, 512))
+    im.paste(im_input, (0, 0))
+    im.paste(im_noise, (768, 0))
+    im.paste(im_t0, (1536, 0))
+    im.save(f'/content/drive/MyDrive/stablediff/denoising2/{i:04}.jpg')
